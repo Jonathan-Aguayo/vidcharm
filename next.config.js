820dc85ba5854c61348c1f196fb9a7902215a38f
iamkennethhuang <kenneth.j.huang@sjsu.edu>
2021-11-08 14:17:07 -0800

    added configure file and corrected mysql configuration issue

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      'MYSQL_HOST': 'rds-mysql-vidcharm.cww4xtjo4mzk.us-west-1.rds.amazonaws.com',
+      'MYSQL_PORT': '3306',
+      'MYSQL_DATABASE': 'vidcharm',
+      'MYSQL_USER': 'admin',
+      'MYSQL_PASSWORD': 'cmpe172vidcharm',
+    }
+}
