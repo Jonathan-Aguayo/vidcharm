101f0c4092de9739c51af9f6b4daa8e33dc5060d
iamkennethhuang <kenneth.j.huang@sjsu.edu>
2021-11-18 13:06:37 -0800

    updated homepage ui design

--- components/VidList.jsx
+++ components/VidList.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Vid from './Vid'
+import { Box, Heading, SimpleGrid } from "@chakra-ui/layout";
 
 export default function VidLlist(props)
 {
         return(
-            <div className="container">
-                <div className="row">
+            // <div className="container">
+            <Box width="100%" mx="auto" my={4}>
+                <SimpleGrid columns={[1,2,3]} spacing={6}>
                     {props.list.map(item => (
-                        <Vid title={item.title} url='watch/{item.vidUrl}' image={item.posterUrl} channel={item.author.email}></Vid>
+                        <Vid title={item.title} url='watch/{item.vidUrl}' image={item.posterUrl} channel={item.author.email} author={item.author}></Vid>
                     ))}
-                </div>
-            </div>  
+                </SimpleGrid>
+            </Box>
+                // <div className="row">
+                //     {props.list.map(item => (
+                //         <Vid title={item.title} url='watch/{item.vidUrl}' image={item.posterUrl} channel={item.author.email}></Vid>
+                //     ))}
+                // </div>
+            // </div>  
         );
 }
